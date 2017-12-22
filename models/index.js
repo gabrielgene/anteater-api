ff40006be4d21f754aaf9bd2a3f136987fd33da6
Gabriel Genê <gabrielgene97@gmail.com>
2017-12-21 23:23:54 -0300

    Update model

--- models/index.js
+++ models/index.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const Data = new mongoose.Schema({
-  cords: { type: String },
-  clime: { type: String },
+  point: { type: Object },
+  weather: { type: String },
 });
 
 module.exports =  mongoose.model('Data', Data);
