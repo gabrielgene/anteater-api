b6a4394b0a8427d5962a46db8d29a1c131da811d
ravi <rafael.vitor@jusbrasil.com.br>
2017-12-23 02:00:48 -0300

    Add createdAt and author to model

--- models/index.js
+++ models/index.js
@@ -4,6 +4,8 @@ const Data = new mongoose.Schema({
   coords: { type: Object },
   weather: { type: String },
   zoom: { type: Number },
+  createdAt: { type: Date, default: Date.now },
+  author: { type: String },
 });
 
 module.exports = mongoose.model('Data', Data);
